4f9ed8b1a14bce50ec66a728bb5d6283bf556609
Kevin Collazos <kevcollazos@gmail.com>
2024-04-13 13:30:10 -0500

    :sparkles: feat(Social): added social section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
 import { Badge } from '@/components/';
+import { Github, Linkedin, Mail, Twitter } from 'lucide-react';
 
 export default function Home() {
+  const links = [
+    {
+      title: 'LinkedIn',
+      url: 'https://linkedin.com/in/kevcollazos/',
+      icon: <Linkedin size={16} />,
+    },
+    {
+      title: 'GitHub',
+      url: 'https://github.com/xKeCo',
+      icon: <Github size={16} />,
+    },
+    {
+      title: 'Twitter',
+      url: 'https://x.com/kcollazos_',
+      icon: <Twitter size={16} />,
+    },
+    {
+      title: 'hi@kcollazos.com',
+      url: 'mailto:kevcollazos@gmail.com',
+      icon: <Mail size={16} />,
+    },
+  ];
+
   return (
     <main className="flex flex-col items-start justify-center flex-none gap-0 h-min overflow-visible">
       <div className="flex flex-col items-start justify-center gap-20 pb-20">
@@ -19,31 +43,20 @@ export default function Home() {
             <br />
             Developing impactful digital solutions that enable people to work effectively and
             cultivate a strong online presence.
-            {/* Enjoying being able to create{' '}
-            <span className="text-neutral-100">crafting engaging digital solutions</span> that
-            empower people to work productively and establish their presence in the{' '}
-            <span className="text-neutral-100">digital world.</span> */}
           </p>
         </div>
       </div>
 
       {/* Social */}
-      {/* <div className="flex flex-col items-start justify-center gap-6">
+      <div className="flex flex-col items-start justify-center gap-6">
         <h2 className="text-base font-medium leading-6 text-neutral-100">Social</h2>
 
         <div className="flex items-center justify-start gap-4">
-          <Badge />
-          <a
-            href="
-            https://www.linkedin.com/in/kevincollazos/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-neutral-100"
-          >
-            LinkedIn
-          </a>
+          {links.map((link) => (
+            <Badge key={link.url} title={link.title} url={link.url} icon={link.icon} />
+          ))}
         </div>
-      </div> */}
+      </div>
     </main>
   );
 }
--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,7 +1,19 @@
-export const Badge = () => {
+type TBadgeProps = {
+  title: string;
+  url: string;
+  icon: any;
+};
+
+export const Badge = ({ title, url, icon }: TBadgeProps) => {
   return (
-    <div>
-      <h1>Badge</h1>
-    </div>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center py-1 px-2 gap-2 text-sm font-medium bg-white/5 rounded-2xl hover:text-black hover:bg-neutral-100 transition-colors duration-200 ease-in-out"
+    >
+      {icon}
+      {title}
+    </a>
   );
 };
